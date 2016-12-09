a6ecadc09d7b8d4768b9e33fb308cee861461d62
Khyon <mayitosonrics@hotmai.com>
2016-12-09 13:20:42 -0600

    Agregado Mapeo y Pool

---
+++ src/mapeo/MapeadorObjetoRelacional.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mapeo;
+
+import mapeo.configuracion.ParseadorConfiguracionMapeo;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import mapeo.excepcion.ArchivoConfiguracionNoEncontradoExcepcion;
+import mapeo.excepcion.MapeoErroneoExcepcion;
+import mapeo.excepcion.MapeoInexistenteExcepcion;
+import mapeo.excepcion.ObjetoErroneoExcepcion;
+import pool.PoolConnection;
+
+/**
+ *
+ * @author Noe
+ *//**
+ *
+ * @author Noe
+ */
+public class MapeadorObjetoRelacional {
+
+    private ArrayList listaObjetos;
+    private MapeoClaseTabla[] mapeos;
+
+    public MapeadorObjetoRelacional(String URLArchivoConfiguracion) throws ArchivoConfiguracionNoEncontradoExcepcion {
+        obtenerMapeos(URLArchivoConfiguracion);
+    }
+
+    private void obtenerMapeos(String URLArchivoConfiguracion) throws ArchivoConfiguracionNoEncontradoExcepcion {
+        ParseadorConfiguracionMapeo parseador = new ParseadorConfiguracionMapeo();
+        mapeos = parseador.parsear(URLArchivoConfiguracion);
+    }
+
+    public void mapearObjetosRelacion(String URLClase, PoolConnection conexion) throws MapeoInexistenteExcepcion, ObjetoErroneoExcepcion, MapeoErroneoExcepcion, SQLException  {
+
+        MapeoClaseTabla mapeoEncontrado = encontrarMapeo(URLClase);
+        
+        if (mapeoEncontrado != null) {
+            String consulta = "SELECT * FROM " + mapeoEncontrado.getNombreTabla();
+            ResultSet datosTabla = conexion.query(consulta);
+            
+            GeneradorObjetos generador = new GeneradorObjetos(mapeoEncontrado, datosTabla);
+            generador.generarObjetos();
+            listaObjetos = generador.getListaObjetos();
+        }else{
+            throw new MapeoInexistenteExcepcion("No existe un mapeo para la clase: "+ URLClase);
+        }
+
+    }
+
+    private MapeoClaseTabla encontrarMapeo(String clase) {
+        
+         for (int j = 0; j < mapeos.length; j++) {
+
+            if ((mapeos[j].getNombreClase()).equals(clase)) {
+                return mapeos[j];
+            }
+
+        }
+        return null;
+
+    }
+
+    public ArrayList getListaObjetos() {
+        return listaObjetos;
+    }
+
+}
---
+++ src/pool/PoolConnection.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pool;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author miguelangel
+ */
+public class PoolConnection {
+    private boolean active;
+    private Connection connection;
+    
+    public PoolConnection(Connection connection){
+        this.active = true;
+        this.connection = connection;
+    }
+    
+    public ResultSet query(String query) throws SQLException{
+        ResultSet result = null;
+        PreparedStatement ps = connection.prepareStatement(query);
+        result = ps.executeQuery();
+        return result; 
+    }
+    
+    public void close(){
+        this.active = false;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+    
+    public void activate(){
+        this.active = true;
+    }
+}
